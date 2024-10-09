fcda509794e7c62cfaf87cffdbe5e51acf227c3f
CodeByDani <danialabedini1380@gmail.com>
2024-10-09 14:52:36 +0330

    Update - Aspire : Add DataBases

--- DShop.AppHost/Program.cs
+++ DShop.AppHost/Program.cs
@@ -3,13 +3,27 @@ var userCatalog = builder.AddParameter("User", true);
 var passCatalog = builder.AddParameter("Pass", true);
 
 //TODO Tools
+// Postgres
 var postgres = builder
     .AddPostgres("postgres", userCatalog, passCatalog, 5432)
     .WithBindMount("./data/CatalogDb", "/var/lib/postgresql/data")
     .WithPgWeb();
 var catalogDb = postgres.AddDatabase("CatalogDb");
+
+//mongo
+var basketDb = builder.AddMongoDB("mongo", 27017)
+    .WithBindMount("./data/BasketDb", "/data/db")
+    .AddDatabase("Basket");
+
+//redis 
+var cache = builder.AddRedis("cache", 6379)
+    .WithImage("ghcr.io/microsoft/garnet")
+    .WithImageTag("latest");
+
 //TODO Projects
 builder.AddProject<Projects.Catalog_API>("catalog-api")
 .WithReference(catalogDb);
+
+builder.AddProject<Projects.Basket_API>("basket-api");
 
 builder.Build().Run();
